7ea1c16551282057d2715213445f0050fc2617bf
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-08-17 09:33:32 +0100

    feat/modified files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { ComposableMap, Geographies, Geography, Marker } from "react-simple-maps"
+import { useState, useEffect} from 'react'
+
+
+const geoUrl = "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json";
+const meteoriteDataUrl = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json";
+
 
 function App() {
+
+  const [meteoriteData, setMeteoriteData] = useState([]);
+  const [coordinate, setCoordinates] = useState([]);
+
+  useEffect(() => {
+    // Fetch meteorite strike data
+    fetch(meteoriteDataUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setMeteoriteData(data.features)
+      });
+  }, [meteoriteData]);
+console.log(meteoriteData.map(features => features.geometry))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+     <ComposableMap >
+      <Geographies 
+      geography={geoUrl}>
+      {({ geographies }) => 
+        geographies.map((geo) => (
+          <Geography  key={geo.rsmkey} geography={geo} />
+        ))
+        }
+      </Geographies>
+     
+            {meteoriteData.map((meteorite) => {
+              const [longitude, latitude] = meteorite.geometry.coordinates;
+              return (
+                <Marker key={meteorite.properties.id} coordinates={[longitude, latitude]}>
+                  <circle r={4} fill="#F00" />
+                </Marker>
+              );
+            })}
+        
+     </ComposableMap>
+      </div>
     </div>
   );
 }
